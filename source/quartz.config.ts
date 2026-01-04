d815e6bf601ca703c08fdb8dadbfc6d04dfd2c28
RM20 <43253883+rm20killer@users.noreply.github.com>
2026-01-04 14:52:05 +0000

    updated default links

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🎣 Anglers-Almanac",
+    pageTitle: "🎣 Angler's Almanac",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
